06f4feeb78ff00746b2ffb7626ab5e81b591ac61
faiz636 <muhammad_faiz@live.com>
2015-10-17 12:29:26 +0500

    some basic code added

--- JavaFX Sample/src/sample/Main.java
+++ JavaFX Sample/src/sample/Main.java
@@ -6,7 +6,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -25,11 +28,53 @@ public class Main extends Application {
 //        code1(primaryStage);
 //        code2(primaryStage);
 //        code3(primaryStage);
-        code4(primaryStage);
+//        code4(primaryStage);
 //        code5(primaryStage);
+        code6(primaryStage);
 
     }
+    
+//    property binding
+//    centring circle on resizing of screen
+    private void code6(Stage primaryStage){
+        // Create a pane to hold the circle
+        Pane pane = new Pane();
 
+        // Create a circle and set its properties
+        Circle circle = new Circle();
+        circle.centerXProperty().bind(pane.widthProperty().divide(2));
+        circle.centerYProperty().bind(pane.heightProperty().divide(2));
+        circle.setRadius(50);
+        circle.setStroke(Color.BLACK);
+        circle.setFill(Color.WHITE);
+        pane.getChildren().add(circle); // Add circle to the pane
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(pane, 200, 200);
+        primaryStage.setTitle("ShowCircleCentered"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+    private void code5(Stage primaryStage) {
+        // Create a circle and set its properties
+        Circle circle = new Circle();
+        circle.setCenterX(100);
+        circle.setCenterY(100);
+        circle.setRadius(50);
+        circle.setStroke(Color.BLACK);
+        circle.setFill(Color.WHITE);
+
+        // Create a pane to hold the circle
+        Pane pane = new Pane();
+        pane.getChildren().add(circle);
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(pane, 200, 200);
+        primaryStage.setTitle("ShowCircle"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
     private void code1(Stage primaryStage) {
         //compiler generated code
         Parent root = null;

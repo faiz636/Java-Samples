aec17e400423c246b2677447a2816c03a2c71271
faiz636 <muhammad_faiz@live.com>
2015-10-25 00:03:19 +0500

    some correction

--- Testing/src/Test.java
+++ Testing/src/Test.java
@@ -1,3 +1,4 @@
+import java.sql.Time;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -11,7 +12,8 @@ public class Test {
             Executors.newScheduledThreadPool(1);
 
     public static void main(String[] args) {
-        beepForAnHour();
+//        beepForAnHour();
+        task();
     }
 
     public static void beepForAnHour() {
@@ -25,9 +27,23 @@ public class Test {
             public void run() {
                 beeperHandle.cancel(true);
                 System.out.println("terminator");
+                //shutting down the scheduler is necessary else it program will not terminate.
+                scheduler.shutdown();
             }
         };
         scheduler.schedule(beepTerminator, 20, TimeUnit.SECONDS);
+    }
+
+    public static void task(){
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("task running");
+                //shutting down the scheduler is necessary else it program will not terminate.
+                scheduler.shutdown();
+            }
+        };
+        scheduler.schedule(task,10, TimeUnit.SECONDS);
     }
 
 }

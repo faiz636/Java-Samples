3f2b127cdff0c76b1383c0d81478358906549dbd
faiz636 <muhammad_faiz@live.com>
2015-12-24 03:53:54 +0500

    Reading Excel file periodically over a fixed period

--- ExcleToFirebase/src/com/rubbersoft/Main.java
+++ ExcleToFirebase/src/com/rubbersoft/Main.java
@@ -2,10 +2,20 @@ package com.rubbersoft;
 
 import com.rubbersoft.model.SheetData;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
+    private static final int INITIAL_DELAY = 5,PERIOD = 5;
+
+    private static final ScheduledExecutorService scheduler =
+            Executors.newScheduledThreadPool(1);
+
     public static void main(String[] args) {
-	// write your code here
+
+/*
         SheetData sheetData = ExcelToJava.readFile("sheet.xls");
         System.out.println(sheetData.size());
 
@@ -16,5 +26,26 @@ public class Main {
             }
             System.out.println();
         }
+*/
+
+        fileReaderTask();
     }
+
+//    this will read the file periodically and print its size
+    public static void fileReaderTask(){
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                SheetData sheetData = ExcelToJava.readFile("sheet.xls");
+                System.out.println("task running : "+sheetData.size());
+
+                //shutting down the scheduler is necessary else it program will not terminate.
+//                scheduler.shutdown();
+            }
+        };
+
+//        scheduler.schedule(task,100, TimeUnit.MILLISECONDS);
+        scheduler.scheduleAtFixedRate(task,INITIAL_DELAY,PERIOD, TimeUnit.SECONDS);
+    }
+
 }

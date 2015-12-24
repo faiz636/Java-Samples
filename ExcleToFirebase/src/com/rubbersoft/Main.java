cba6ef7ad764187edce99783c284abd8317ae904
faiz636 <muhammad_faiz@live.com>
2015-12-24 18:41:22 +0500

    send data to firebse and if new rows are not there will not run FirebaseThread.

--- ExcleToFirebase/src/com/rubbersoft/Main.java
+++ ExcleToFirebase/src/com/rubbersoft/Main.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    private static final int INITIAL_DELAY = 5,PERIOD = 10;
+    private static final int INITIAL_DELAY = 5, PERIOD = 20;
 
     private static final ScheduledExecutorService scheduler =
             Executors.newScheduledThreadPool(1);
@@ -17,21 +17,24 @@ public class Main {
         fileReaderTask();
     }
 
-//    this will read the file periodically and print its size
-    public static void fileReaderTask(){
+    //    this will read the file periodically and print its size
+    public static void fileReaderTask() {
         Runnable task = new Runnable() {
             @Override
             public void run() {
                 SheetData sheetData = ExcelToJava.readFile("sheet.xls");
-                System.out.println("task running : "+sheetData.size());
-
+                if (sheetData == null) return;
+                System.out.println("rows added in excle file : " + sheetData.size());
+                if (sheetData.size() > 0) {
+                    new FirebaseThread(sheetData);
+                }
                 //shutting down the scheduler is necessary else it program will not terminate.
 //                scheduler.shutdown();
             }
         };
 
 //        scheduler.schedule(task,100, TimeUnit.MILLISECONDS);
-        scheduler.scheduleAtFixedRate(task,INITIAL_DELAY,PERIOD, TimeUnit.SECONDS);
+        scheduler.scheduleAtFixedRate(task, INITIAL_DELAY, PERIOD, TimeUnit.SECONDS);
     }
 
 }

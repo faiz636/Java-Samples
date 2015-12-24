0cf0b64e354452029d1ee5f964079288f12d832a
faiz636 <muhammad_faiz@live.com>
2015-12-24 18:38:07 +0500

    Don't add row for upload if it contains empty cell

--- ExcleToFirebase/src/com/rubbersoft/ExcelToJava.java
+++ ExcleToFirebase/src/com/rubbersoft/ExcelToJava.java
@@ -10,15 +10,15 @@ import java.io.IOException;
 
 public class ExcelToJava {
 
+    private static int START_POINT = 1;
+
     /**
      * Returns the data of Excel file as an object.
      *
      * @param path Path of the file i.e sheet.xls
-     * @return the data present in the file
+     * @return data present in the file
+     *         null if an exception occured
      */
-
-    private static int START_POINT;
-
     public static SheetData readFile(String path) {
         try {
             Workbook workbook = Workbook.getWorkbook(new File(path));
@@ -26,17 +26,28 @@ public class ExcelToJava {
             SheetData sheetData= new SheetData();
 
             int size = workbook.getSheet(0).getRows();
+            boolean invalidData;
+            System.out.println("reading excle file");
 
             for(int i=START_POINT; i<size; i++){
                 SheetRow sheetRow = new SheetRow();
+                invalidData = false;
                 for (int j=0; j<=7; j++){
                     /*
                     This will print each cell before adding it into the column..
                     if(j==0) System.out.println();
                     System.out.print(sheet.getCell(j,i).getContents() + "   ");
                     */
-                    sheetRow.setColumn(sheet.getCell(j,i).getContents());
+                    String s = sheet.getCell(j,i).getContents();
+                    if (s.length()==0) {
+                        invalidData = true;
+                        System.out.println("row "+ i + " is invalid");
+                        break;
+                    }
+
+                    sheetRow.setColumn(s);
                 }
+                if (invalidData) continue;
                 sheetData.addRow(sheetRow);
             }
             START_POINT = size;

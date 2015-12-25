08eab192f5383e43a1fcd83174993ebb172fd446
faiz636 <muhammad_faiz@live.com>
2015-12-25 23:40:08 +0500

    Initializing Android Project

---
+++ Android/ValveLeakage/app/src/main/java/com/rubbersoft/android/valveleakage/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rubbersoft.android.valveleakage.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rubbersoft.android.valveleakage.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

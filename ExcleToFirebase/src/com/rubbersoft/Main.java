adb3d58fcd6cd647b98647d66e09db99b8e6504b
faiz636 <muhammad_faiz@live.com>
2015-12-25 21:18:09 +0500

    Scheduler added
    Thread Return :D

--- ExcleToFirebase/src/com/rubbersoft/Main.java
+++ ExcleToFirebase/src/com/rubbersoft/Main.java
@@ -3,13 +3,25 @@ package com.rubbersoft;
 import com.rubbersoft.model.SheetData;
 
 import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    private static final int INITIAL_DELAY = 5, PERIOD = 20;
+    private static final int INITIAL_DELAY = 0, PERIOD = 1;
+    private static final ScheduledExecutorService scheduler =
+            Executors.newScheduledThreadPool(5);
 
     public static void main(String[] args) {
-        fileReaderTask();
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("running file reader task");
+                fileReaderTask();
+            }
+        };
+        scheduler.scheduleAtFixedRate(task, Main.INITIAL_DELAY, Main.PERIOD, TimeUnit.MINUTES);
     }
 
     //    this will read the file periodically and print its size
@@ -21,7 +33,7 @@ public class Main {
         t = System.currentTimeMillis();
         FirebaseDataSender ft = new FirebaseDataSender(sheetDataList);
         System.out.println("sending data total time taken " + (System.currentTimeMillis() - t) + "ms, " + ((System.currentTimeMillis() - t) / (1000)) + "sec");
-        System.exit(0);
+//        System.exit(0);
     }
 
 }

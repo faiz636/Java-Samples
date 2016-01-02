d68b10f3f0fde7b914d96a8f42373887013219e5
faiz636 <muhammad_faiz@live.com>
2016-01-02 12:07:13 +0500

    working on displaying notification

--- Android/ValveLeakage/app/src/main/java/com/rubbersoft/android/valveleakage/services/CoreLeakageService.java
+++ Android/ValveLeakage/app/src/main/java/com/rubbersoft/android/valveleakage/services/CoreLeakageService.java
@@ -1,14 +1,20 @@
 package com.rubbersoft.android.valveleakage.services;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Query;
+import com.rubbersoft.android.valveleakage.R;
 import com.rubbersoft.android.valveleakage.ValveLeakageApplication;
 import com.rubbersoft.android.valveleakage.firebase.ChildEventListenerAdapter;
 import com.rubbersoft.android.valveleakage.firebase.FirebaseHandler;
@@ -18,39 +24,40 @@ import com.rubbersoft.android.valveleakage.utils.ConfigConstants;
 import com.rubbersoft.android.valveleakage.utils.DataBaseSource;
 import com.rubbersoft.android.valveleakage.utils.SharedPreferenceManager;
 
-import java.util.Calendar;
-
 public class CoreLeakageService extends Service {
 
-    private IBinder mBinder = new LocalBinder();
-    private ServiceCallBacks mserviceCallBacks;     //Use this object to call populateListView method in the activity class..
     Intent node1receiverIntent = new Intent(ConfigConstants.RECEIVER_ACTION_NODE1);
     Intent node2receiverIntent = new Intent(ConfigConstants.RECEIVER_ACTION_NODE2);
     Intent node3receiverIntent = new Intent(ConfigConstants.RECEIVER_ACTION_NODE3);
     Intent node4receiverIntent = new Intent(ConfigConstants.RECEIVER_ACTION_NODE4);
-
     DataBaseSource dataBaseSource;
     FirebaseHandler firebaseHandler;
     SharedPreferenceManager sharedPreferenceManager;
     long startingTimeStamp;
     Query ref;
+    PendingIntent pendingIntentNode1,
+            pendingIntentNode2,
+            pendingIntentNode3,
+            pendingIntentNode4;
     ChildEventListener node1ChildEventListener,
             node2ChildEventListener,
             node3ChildEventListener,
             node4ChildEventListener;
+    private IBinder mBinder = new LocalBinder();
 
     public CoreLeakageService() {
         dataBaseSource = ValveLeakageApplication.getDataBaseSource();
         firebaseHandler = ValveLeakageApplication.getFirebaseHandler();
         sharedPreferenceManager = ValveLeakageApplication.getSharedPreferenceManager();
-        createChildEventListeners();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        Log.d("FBLOG",  "in onStartCommand");
+        Log.d("FBLOG", "in onStartCommand");
 
+        initPendingIntents();
+        createChildEventListeners();
         implementFirebaseListeners();
 
         return START_STICKY;
@@ -68,7 +75,7 @@ public class CoreLeakageService extends Service {
     }
 
     private void implementFirebaseListeners() {
-        Log.d("FBLOG",  "in implementFirebaseListeners");
+        Log.d("FBLOG", "in implementFirebaseListeners");
         firebaseHandler.getNode1Ref().orderByChild("timestamp").addChildEventListener(node1ChildEventListener);
         firebaseHandler.getNode2Ref().orderByChild("timestamp").addChildEventListener(node2ChildEventListener);
         firebaseHandler.getNode3Ref().orderByChild("timestamp").addChildEventListener(node3ChildEventListener);
@@ -85,71 +92,134 @@ public class CoreLeakageService extends Service {
         return false;
     }
 
-    public class LocalBinder extends Binder{
-        CoreLeakageService getService(){
-            return CoreLeakageService.this;
-        }
-    }
-
-    public static interface ServiceCallBacks {
-        public void populateListView();
-    }
-
-    public void setCallBacksForService(ServiceCallBacks serviceCallBacks){
-        this.mserviceCallBacks = serviceCallBacks;
-    }
-
-    private void createChildEventListeners(){
-        node1ChildEventListener  = new ChildEventListenerAdapter() {
+    private void createChildEventListeners() {
+        node1ChildEventListener = new ChildEventListenerAdapter() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Data data = dataSnapshot.getValue(Data.class);
                 dataBaseSource.insertData(data, ConfigConstants.TABLE_NODE1);
                 sendBroadcast(node1receiverIntent);
 
-//            TODO: Call your notification genenartor method here
-
+                if (data.getLPGConcentration() >= 200) {
+                    generateNotification(1, "Node 1", data.getTimestamp(), pendingIntentNode1);
+                }
                 Log.d("FBLOG", data.getTimestamp() + "");
             }
         };
 
-        node2ChildEventListener  = new ChildEventListenerAdapter() {
+        node2ChildEventListener = new ChildEventListenerAdapter() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Data data = dataSnapshot.getValue(Data.class);
                 dataBaseSource.insertData(data, ConfigConstants.TABLE_NODE2);
                 sendBroadcast(node2receiverIntent);
 
-//            TODO: Call your notification genenartor method here
-
+                if (data.getLPGConcentration() >= 200) {
+                    generateNotification(2, "Node 2", data.getTimestamp(), pendingIntentNode2);
+                }
                 Log.d("FBLOG", data.getTimestamp() + "");
             }
         };
 
-        node3ChildEventListener  = new ChildEventListenerAdapter() {
+        node3ChildEventListener = new ChildEventListenerAdapter() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Data data = dataSnapshot.getValue(Data.class);
                 dataBaseSource.insertData(data, ConfigConstants.TABLE_NODE3);
                 sendBroadcast(node3receiverIntent);
 
-//            TODO: Call your notification genenartor method here
-
+                if (data.getLPGConcentration() >= 200) {
+                    generateNotification(3, "Node 3", data.getTimestamp(), pendingIntentNode3);
+                }
                 Log.d("FBLOG", data.getTimestamp() + "");
             }
         };
 
-        node4ChildEventListener  = new ChildEventListenerAdapter() {
+        node4ChildEventListener = new ChildEventListenerAdapter() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Data data = dataSnapshot.getValue(Data.class);
                 dataBaseSource.insertData(data, ConfigConstants.TABLE_NODE4);
                 sendBroadcast(node4receiverIntent);
 
-//            TODO: Call your notification genenartor method here
-
+                if (data.getLPGConcentration() >= 200) {
+                    generateNotification(4, "Node 4", data.getTimestamp(), pendingIntentNode4);
+                }
                 Log.d("FBLOG", data.getTimestamp() + "");
             }
         };
     }
+
+    private void generateNotification(int i, String s, long when, PendingIntent pendingIntent) {
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+
+        notificationBuilder.setContentTitle(s + " Leakage")
+                .setContentText("Take necessary measures")
+                .setTicker(s + " Is Leaking")
+                .setWhen(when)
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setSmallIcon(R.drawable.ic_stat)
+//                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentIntent(pendingIntent)
+//                .setColor(0xF44336)
+//                .setContentInfo("Information")
+                .setAutoCancel(true)
+//                .addAction(R.drawable.icon_small, "First Action", pendingIntent)
+        ;
+/*
+//        set Visibility of your notification
+        notificationBuilder.setVisibility(Notification.VISIBILITY_PRIVATE)
+//                if it is set to PRIVATE this notification will be shown on lock screen
+//                then set a public version of your notification which will be shown on
+//                unlocked device which will contain all information
+                .setPublicVersion(publicNotification);
+*/
+
+/*
+//        building expanded inbox style notification
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        inboxStyle.setBigContentTitle("Now This BIG Content Title");
+        inboxStyle.setSummaryText("This Is Summary Text");
+//        adding line for each message
+        for(int i=0;i<5;i++){
+            inboxStyle.addLine("Inbox Message "+i);
+        }
+//        setting expanded notification style
+        notificationBuilder.setStyle(inboxStyle);
+*/
+
+//        code for android device older then JELLY_BEAN
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            notificationBuilder.setPriority(Notification.PRIORITY_HIGH);
+        }
+
+
+//        obtaining system
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+//        notifying on same notification id
+//        if there is no previous notification of this id a new will generate
+//        else the previous will be updated to newer notification
+//        so there will be no duplicate notification
+        notificationManager.notify(i, notificationBuilder.build());
+    }
+
+    private void initPendingIntents() {
+        pendingIntentNode1 = PendingIntent.getActivity(this, 1, new Intent(this, MainActivity.class).putExtra(ConfigConstants.INTENT_EXTRA_NODE, ConfigConstants.TABLE_NODE1), 0);
+        pendingIntentNode2 = PendingIntent.getActivity(this, 1, new Intent(this, MainActivity.class).putExtra(ConfigConstants.INTENT_EXTRA_NODE, ConfigConstants.TABLE_NODE2), 0);
+        pendingIntentNode3 = PendingIntent.getActivity(this, 1, new Intent(this, MainActivity.class).putExtra(ConfigConstants.INTENT_EXTRA_NODE, ConfigConstants.TABLE_NODE3), 0);
+        pendingIntentNode4 = PendingIntent.getActivity(this, 1, new Intent(this, MainActivity.class).putExtra(ConfigConstants.INTENT_EXTRA_NODE, ConfigConstants.TABLE_NODE4), 0);
+    }
+
+    public static interface ServiceCallBacks {
+        public void populateListView();
+    }
+
+    public class LocalBinder extends Binder {
+        CoreLeakageService getService() {
+            return CoreLeakageService.this;
+        }
+    }
+
 }
--- Android/ValveLeakage/app/src/main/java/com/rubbersoft/android/valveleakage/ui/MainActivity.java
+++ Android/ValveLeakage/app/src/main/java/com/rubbersoft/android/valveleakage/ui/MainActivity.java
@@ -1,28 +1,11 @@
 package com.rubbersoft.android.valveleakage.ui;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.NotificationCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.rubbersoft.android.valveleakage.R;
-import com.rubbersoft.android.valveleakage.ValveLeakageApplication;
-import com.rubbersoft.android.valveleakage.model.ListAdapter;
-import com.rubbersoft.android.valveleakage.utils.ConfigConstants;
-import com.rubbersoft.android.valveleakage.utils.DataBaseSource;
-import com.rubbersoft.android.valveleakage.utils.SharedPreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
 //    ListAdapter listAdapter;
 //    DataBaseSource dataBaseSource;
 //    SharedPreferenceManager sharedPreferenceManager;
-//    PendingIntent pendingIntent;
 
     ViewPager viewPager;
     PagerAdapter pagerAdapter;
@@ -48,75 +30,6 @@ public class MainActivity extends AppCompatActivity {
         pagerAdapter = new PagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(pagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
-
-//        Intent intent = new Intent(getApplicationContext(), CoreLeakageService.class);
-//       startService(intent);
-//
-//        pendingIntent = PendingIntent.getActivity(MainActivity.this, 1,
-//                new Intent(MainActivity.this, MainActivity.class), 0);
-//        ((Button)findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(MainActivity.this, "generating notification" + i, Toast.LENGTH_SHORT).show();
-//                generateNotification("", pendingIntent);
-//            }
-//        });
-
-    }
-
-    private void generateNotification(String s, PendingIntent pendingIntent) {
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
-
-        notificationBuilder.setContentTitle("TITLE")
-                .setContentText("Content text " + j++)
-                .setTicker("ticker")
-                .setWhen(System.currentTimeMillis())
-//                .setDefaults(Notification.DEFAULT_SOUND)
-                .setSmallIcon(R.drawable.ic_stat)
-//                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
-                .setContentIntent(pendingIntent)
-//                .setColor(0xF44336)
-//                .setContentInfo("Information")
-                .setAutoCancel(true)
-                .addPerson("Faizan")
-//                .addAction(R.drawable.icon_small, "First Action", pendingIntent)
-        ;
-/*
-//        set Visibility of your notification
-        notificationBuilder.setVisibility(Notification.VISIBILITY_PRIVATE)
-//                if it is set to PRIVATE this notification will be shown on lock screen
-//                then set a public version of your notification which will be shown on
-//                unlocked device which will contain all information
-                .setPublicVersion(publicNotification);
-*/
-
-/*
-//        building expanded inbox style notification
-        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
-        inboxStyle.setBigContentTitle("Now This BIG Content Title");
-        inboxStyle.setSummaryText("This Is Summary Text");
-//        adding line for each message
-        for(int i=0;i<5;i++){
-            inboxStyle.addLine("Inbox Message "+i);
-        }
-//        setting expanded notification style
-        notificationBuilder.setStyle(inboxStyle);
-*/
-
-//        code for android device older then JELLY_BEAN
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            notificationBuilder.setPriority(Notification.PRIORITY_HIGH);
-        }
-
-
-//        obtaining system
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-//        notifying on same notification id
-//        if there is no previous notification of this id a new will generate
-//        else the previous will be updated to newer notification
-//        so there will be no duplicate notification
-        notificationManager.notify(i, notificationBuilder.build());
     }
 
 }

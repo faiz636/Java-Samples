027bbc453f93e07a08cb445d8777af675bdba92f
faiz636 <muhammad_faiz@live.com>
2016-01-02 18:51:09 +0500

    seprated process for service handling notification intent(pending)

--- Android/ValveLeakage/app/src/main/java/com/rubbersoft/android/valveleakage/ui/MainActivity.java
+++ Android/ValveLeakage/app/src/main/java/com/rubbersoft/android/valveleakage/ui/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.rubbersoft.android.valveleakage.R;
+import com.rubbersoft.android.valveleakage.utils.AppLog;
+import com.rubbersoft.android.valveleakage.utils.ConfigConstants;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +32,30 @@ public class MainActivity extends AppCompatActivity {
         pagerAdapter = new PagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(pagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+        changeTabForIntentExtras();
+    }
+
+    private void changeTabForIntentExtras(){
+
+        String nodeName = getIntent().getStringExtra(ConfigConstants.INTENT_EXTRA_NODE);
+        if (nodeName!=null) {
+            switch (nodeName) {
+                case ConfigConstants.TABLE_NODE1:
+                    viewPager.setCurrentItem(0);
+                    break;
+                case ConfigConstants.TABLE_NODE2:
+                    viewPager.setCurrentItem(1);
+                    break;
+                case ConfigConstants.TABLE_NODE3:
+                    viewPager.setCurrentItem(2);
+                    break;
+                case ConfigConstants.TABLE_NODE4:
+                    viewPager.setCurrentItem(3);
+                    break;
+                default:
+                    AppLog.e("MuzammilQadri", "insertData() called with wrong Node");
+            }
+        }
     }
 
 }
